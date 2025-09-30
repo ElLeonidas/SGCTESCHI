fe02fce681b3975f395c78f4c5bacc71c4d017b3
LEONARDO DANIEL GARCIA GARCIA <2021452047@teschi.edu.mx>
2025-09-30 11:30:14 -0600

    #LOGIN 1.1

--- SGCTESCHI/Login.Designer.cs
+++ SGCTESCHI/Login.Designer.cs
@@ -97,7 +97,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(24, 13);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(484, 489);
+            this.pictureBox1.Size = new System.Drawing.Size(450, 450);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
