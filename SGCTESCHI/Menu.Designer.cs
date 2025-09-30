93c449eb627af80b5acf6bf8b4a90dcf6a66dce2
DANIEL VELAZQUEZ TREJO <2021452168@teschi.edu.mx>
2025-09-30 13:02:19 -0600

    1.3 menu actualizado asi como login
    ahora yase como funciona el menu eso creo

--- SGCTESCHI/Menu.Designer.cs
+++ SGCTESCHI/Menu.Designer.cs
@@ -31,12 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(1442, -1);
+            this.button1.Location = new System.Drawing.Point(160, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(40, 41);
             this.button1.TabIndex = 0;
@@ -44,6 +45,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(0, -1);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 851);
@@ -55,11 +57,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1482, 850);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Menu";
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.Menu_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
