666af261b1ebde6764a1a0e7d7b253865963155b
DANIEL VELAZQUEZ TREJO <2021452168@teschi.edu.mx>
2025-10-09 09:53:18 -0600

    1.4.6.2 Se agrego una Tabla para visualizar lo que tengamos en los registros
    Ojala ser el metro y Poder ver a Bella Luna

--- SGCTESCHI/Registros.Designer.cs
+++ SGCTESCHI/Registros.Designer.cs
@@ -32,8 +32,14 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID_Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Permisos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Contraseña = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -51,28 +57,73 @@
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Location = new System.Drawing.Point(14, 336);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Location = new System.Drawing.Point(14, 419);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1237, 296);
+            this.panel3.Size = new System.Drawing.Size(1237, 454);
             this.panel3.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(14, 15);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1237, 293);
+            this.panel2.Size = new System.Drawing.Size(1237, 357);
             this.panel2.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 30);
+            this.label1.Location = new System.Drawing.Point(33, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(139, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Consulta de Registros";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID_Usuario,
+            this.Nombre,
+            this.Permisos,
+            this.Contraseña});
+            this.dataGridView1.Location = new System.Drawing.Point(20, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1194, 402);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ID_Usuario
+            // 
+            this.ID_Usuario.HeaderText = "ID_Usuario";
+            this.ID_Usuario.MinimumWidth = 6;
+            this.ID_Usuario.Name = "ID_Usuario";
+            this.ID_Usuario.Width = 125;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre ";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 125;
+            // 
+            // Permisos
+            // 
+            this.Permisos.HeaderText = "Permsos ";
+            this.Permisos.MinimumWidth = 6;
+            this.Permisos.Name = "Permisos";
+            this.Permisos.Width = 125;
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.HeaderText = "Contraseña";
+            this.Contraseña.MinimumWidth = 6;
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Width = 125;
             // 
             // Registros
             // 
@@ -83,8 +134,9 @@
             this.Size = new System.Drawing.Size(1268, 893);
             this.Load += new System.EventHandler(this.Registros_Load);
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -95,5 +147,10 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID_Usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Permisos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Contraseña;
     }
 }
--- SGCTESCHI/Registros.cs
+++ SGCTESCHI/Registros.cs
@@ -26,5 +26,10 @@ namespace SGCTESCHI
         {
 
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
